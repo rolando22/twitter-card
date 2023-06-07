b748fbc09e6868aa52ae1a3d7ff892415e42bc30
rolando22 <rolando2207@gmail.com>
2023-06-07 15:55:59 -0300

    [ADD] App y estilos generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
 export function App() {
-    return (<div></div>);
+    return (
+        <article>
+            <section>
+                <img src='https://unavatar.io/midudev' alt='El avatar de midudev' />
+                <div>
+                    <strong>Miguél Ángel Durán</strong>
+                    <span>@midudev</span>
+                </div>
+            </section>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    );
 }
